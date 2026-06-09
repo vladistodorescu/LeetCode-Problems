3c6ff3fa321da65e281c158e3a79004bce493282
Vlad Istodorescu <166702009+vladistodorescu@users.noreply.github.com>
2026-06-09 12:49:26 +0300

    Final Version

--- Add 2 Numbers/Solution.java
+++ Add 2 Numbers/Solution.java
@@ -8,6 +8,10 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+import java.util.*;
+import java.math.*;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         List<Integer> list1 = new ArrayList<Integer>();     
@@ -27,41 +31,43 @@ class Solution {
         }
         list2.add(currentNodeL2.val);
 
-        Integer nr1 = 0;
-        for (Integer i = list1.size()-1; i >= 0; i--){
-            if (nr1 == 0){
-                nr1 += list1.get(i);
-            }
-            else{
-                nr1 = (nr1 * 10) + list1.get(i);
-            }
+        BigInteger nr1 = BigInteger.ZERO;
+        for (int i = list1.size()-1; i >= 0; i--){
+            BigInteger digit = BigInteger.valueOf(list1.get(i));
+            nr1 = nr1.multiply(BigInteger.TEN).add(digit);
         }
 
-        Integer nr2 = 0;
-        for (Integer i = list2.size()-1; i >= 0; i--){
-            if (nr2 == 0){
-                nr2 += list2.get(i);
-            }
-            else{
-                nr2 = (nr2 * 10) + list2.get(i);
-            }
+        BigInteger nr2 = BigInteger.ZERO;
+        for (int i = list2.size()-1; i >= 0; i--){
+            BigInteger digit = BigInteger.valueOf(list2.get(i));
+            nr2 = nr2.multiply(BigInteger.TEN).add(digit);
         }
+        
+        System.out.println(nr1);
+        System.out.println(nr2);
 
-        Integer finalSum = nr1 + nr2;
+        BigInteger finalSum = nr1.add(nr2);
+        if (finalSum.equals(BigInteger.ZERO)){
+            return new ListNode(0);
+        }
+        System.out.println(finalSum);
+
         ListNode currentNode = new ListNode();
         ListNode finalNode = currentNode;
         ListNode nextNode;
 
-        while (finalSum > 0){
-            Integer currentDigit = finalSum % 10;
+        while (finalSum.compareTo(BigInteger.ZERO) > 0){
+            int currentDigit = finalSum.remainder(BigInteger.TEN).intValue();
             currentNode.val = currentDigit;
-            if (finalSum >= 10) {
+            System.out.println(currentDigit);
+            
+            finalSum = finalSum.divide(BigInteger.TEN);
+
+            if (finalSum.compareTo(BigInteger.ZERO) > 0) {
                 nextNode = new ListNode();
                 currentNode.next = nextNode;
                 currentNode = currentNode.next;
             }
-
-            finalSum = finalSum / 10;
         }
 
 
